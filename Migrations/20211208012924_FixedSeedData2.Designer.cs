a80d9d6550317c1fb1674ceb4029f2e02951e1da
carson29 <carson29@mail.nmc.edu>
2021-12-07 20:29:38 -0500

    Modified seed data

--- Migrations/20211208012924_FixedSeedData2.Designer.cs
+++ Migrations/20211208012924_FixedSeedData2.Designer.cs
@@ -3,14 +3,16 @@ using CapstoneProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CapstoneProject.Migrations
 {
     [DbContext(typeof(PostingContext))]
-    partial class PostingContextModelSnapshot : ModelSnapshot
+    [Migration("20211208012924_FixedSeedData2")]
+    partial class FixedSeedData2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
